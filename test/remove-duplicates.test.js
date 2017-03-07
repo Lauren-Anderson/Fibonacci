86ded3377e9cc618d2819d6bc844f30806d99b6e
raydecastro <captray108@gmail.com>
2017-03-06 21:55:48 -0800

    feature. implemented class, and added failing remove function.

--- test/remove-duplicates.test.js
+++ test/remove-duplicates.test.js
@@ -7,6 +7,12 @@ describe("RemoveDuplicates", () => {
         expect(removeDuplicates).to.be.defined;
     });
 
-    
+    it("shall have the ability to remove duplicates", () => {
+        let rd = new RemoveDuplicates();
+        let inputArray = [0, 0, 1, 1, 2, 2, 3, 3, 4, 5, 5];
+        let expectedOutput = [0, 1, 2, 3, 4, 5];
+
+        expect(rd.remove(inputArray)).to.deep.equal(expectedOutput);
+    })
 
 });

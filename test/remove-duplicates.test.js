0e193b698562f1974d0606fc4635e5759676018e
Lauren Anderson <laurenja88@gmail.com>
2017-03-06 21:50:36 -0800

    Creates test for remove-duplicates

---
+++ test/remove-duplicates.test.js
@@ -0,0 +1,12 @@
+import {expect} from "chai";
+import RemoveDuplicates from "../source/remove-duplicates.js";
+
+describe("RemoveDuplicates", () => {
+    it("shall exist", () => {
+        let removeDuplicates = new RemoveDuplicates();
+        expect(removeDuplicates).to.be.defined;
+    });
+
+    
+
+});

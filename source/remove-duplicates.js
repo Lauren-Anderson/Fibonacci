55865976cd8645544997b99993be2de1f61bb195
Lauren Anderson <laurenja88@gmail.com>
2017-03-07 20:06:19 -0800

    Adds code for remove duplicates challenge

--- source/remove-duplicates.js
+++ source/remove-duplicates.js
@@ -1,5 +1,16 @@
 class RemoveDuplicates {
-
+    remove(arr){
+      var obj = {};
+      var noDuplicateArray = [];
+      for(var i = 0; i < arr.length; i++){
+        var currentIndex = arr[i];
+        if(obj[currentIndex] === undefined){
+          obj[currentIndex] = currentIndex;
+          noDuplicateArray.push(currentIndex);
+        }
+      }
+      return noDuplicateArray;
+    }
 }
 
 export default RemoveDuplicates;
